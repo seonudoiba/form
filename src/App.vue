1b520618094a353a56db73bdedfae7c6ed1275be
seonudoiba <abiodunigbehinadun17@gmail.com>
2022-09-04 23:10:23 +0100

    Vue Calculator

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h2>Guess the a number from 1-100 to Know My Crush </h2>
+    <h4>You have only {{attempt}} left </h4>
+    <form >
+      <h3>Guessed Number: {{guessed}}</h3>
+      <label for="num"><b>Input a Number From 1-100 : </b></label>
+      <input min="1" max="100" type="number" id="num" v-model="guessed"/>
+      <button v-on:click="console.log('Hi')" type="submit" class="btn">Check</button>
+    </form>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      attempt: 5,
+      guessed:"",
+      randomNum: 56,
+      message: "Starr"
+    }
+  },
+  components: {
+  
+  },
+  methods: {
+    submitb() {
+      // attempt = this.attempt - 1
+      // if(this.guessed == !this.randomNum){
+      //   this.message = `You have ${this.attempt} left, Try again`
+      // } else {
+      //   this.message = "You CAn Now Know my crush"
+      // }
+      alert(`Hi ${attempt -1}`)
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 
+  60px;
+}
+.btn{
+  color: red;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>This is a pop up</h2>
+    <button @click="$emit('close')">Close pop up</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld', 
+  props: {
+    msg: String
+  },
+  emits: ['close'],  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
